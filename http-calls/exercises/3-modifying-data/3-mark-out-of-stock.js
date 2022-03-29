cacf428acbe3c622b859df5027862792a8f36992
Sanne Sofie <sannesofie@me.com>
2022-03-29 12:44:20 +0200

    adding exercises

--- http-calls/exercises/3-modifying-data/3-mark-out-of-stock.js
+++ http-calls/exercises/3-modifying-data/3-mark-out-of-stock.js
@@ -4,8 +4,20 @@
  * as out of stock
  * @param {*} productId the id of the product that is out of stuck
  */
-async function markOutOfStock(productId) {
- _
+async function markOutOfStock(productId = 0) {
+    const path = products/$,{productId};
+    const body = {
+    data: {
+        outOfStock: true,
+      }, // question: how can we reach this field when it is inside of 'attributes'?
+    };
+    const url = http//localhost:1337/api/${path};
+    const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+    });
+    return response.json();
 }
-
-markOutOfStock(7);
--- http-calls/exercises/3-modifying-data/4-apply-discount.js
+++ http-calls/exercises/3-modifying-data/4-apply-discount.js
@@ -3,7 +3,21 @@
  * to a product
  */
 async function applyDiscount(productId, discountId) {
-  _
+  const path = products/$,{productId},populate=discountID;
+  const body = {
+    data: {
+      discount: {
+        id: discountId,
+      },
+    },
+  };
+  const url = http//localhost:1337/api/${path};
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  return response.json();
 }
-
-applyDiscount(5, 2);
