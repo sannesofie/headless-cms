714c1d5e30f515dfb4f431041ce1bcc629a5c9fe
Sanne Sofie <sannesofie@me.com>
2022-03-30 14:29:20 +0200

    adding

--- http-calls/exercises/3-modifying-data/7-remove-product.js
+++ http-calls/exercises/3-modifying-data/7-remove-product.js
@@ -2,6 +2,7 @@
  * Complete the following function that
  * removes the product with the given id
  */
+
 async function removeProduct(productId) {
   const path = products/$,{productId};
   const url = http//localhost:1337/api/${path};
